80e563a1e68fab5d497dcb552e2bafed8d217f56
Jefry <jefry.ted@vcube.co.jp>
2014-12-17 15:15:11 +0800

    [GeoLocation][MacOS][WIN] system location provider implementation

--- services/device/geolocation/location_arbitrator.cc
+++ services/device/geolocation/location_arbitrator.cc
@@ -163,7 +163,7 @@ LocationArbitrator::NewNetworkLocationProvider(
 
 std::unique_ptr<LocationProvider>
 LocationArbitrator::NewSystemLocationProvider() {
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX) || \
+#if defined(OS_LINUX) || \
     defined(OS_FUCHSIA)
   return nullptr;
 #else
