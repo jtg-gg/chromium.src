059d580380e3d7f3009342c733d18f2f08650085
Jefry <jefry.ted@vcube.co.jp>
2014-12-17 15:15:11 +0800

    [GeoLocation][MacOS][WIN] system location provider implementation

---
+++ services/device/geolocation/system_location_provider_mac.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
+#define SERVICES_DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
+
+#include "system_location_provider.h"
+
+#endif //SERVICES_DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
