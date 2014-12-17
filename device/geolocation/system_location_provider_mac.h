965401a8d4e11f8fd1d1b2859bd6507f284c0fd7
Jefry <jefry.ted@vcube.co.jp>
2014-12-17 15:15:11 +0800

    [GeoLocation][OSX][WIN] location provider implementation, modify LocationArbitrator to wait and get best data from all data provider, add IP based location

---
+++ device/geolocation/system_location_provider_mac.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
+#define DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
+
+#include "device/geolocation/system_location_provider.h"
+
+#endif  // DEVICE_GEOLOCATION_SYSTEM_LOCATION_PROVIDER_MAC_H_
