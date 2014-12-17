9253c7f8514a3261b07756abd34e5919ba54765a
Jefry <jefry.ted@vcube.co.jp>
2014-12-17 15:15:11 +0800

    [Geolocation][OSX] fix bug on error not being reported (+1 squashed commit) Squashed commits: [994eb60] [GeoLocation] initial commit [OSX] location provider implementation, modify LocationArbitrator to wait and get best data from all data provider

--- content/browser/geolocation/location_arbitrator_impl.cc
+++ content/browser/geolocation/location_arbitrator_impl.cc
@@ -9,6 +9,7 @@
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "content/browser/geolocation/network_location_provider.h"
+#include "content/browser/geolocation/system_location_provider.h"
 #include "content/public/browser/access_token_store.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
@@ -24,7 +25,7 @@ const char* kDefaultNetworkProviderUrl =
 // To avoid oscillations, set this to twice the expected update interval of a
 // a GPS-type location provider (in case it misses a beat) plus a little.
 const int64 LocationArbitratorImpl::kFixStaleTimeoutMilliseconds =
-    11 * base::Time::kMillisecondsPerSecond;
+    2000 * base::Time::kMillisecondsPerSecond;
 
 LocationArbitratorImpl::LocationArbitratorImpl(
     const LocationUpdateCallback& callback)
@@ -79,7 +80,7 @@ void LocationArbitratorImpl::StopProviders() {
   // the newly constructed providers.
   position_provider_ = NULL;
   position_ = Geoposition();
-
+  providers_results_count_.clear();
   providers_.clear();
   is_running_ = false;
 }
@@ -124,14 +125,16 @@ void LocationArbitratorImpl::RegisterProvider(
 
 void LocationArbitratorImpl::OnLocationUpdate(const LocationProvider* provider,
                                               const Geoposition& new_position) {
+  providers_results_count_[provider]++;
   DCHECK(new_position.Validate() ||
          new_position.error_code != Geoposition::ERROR_CODE_NONE);
-  if (!IsNewPositionBetter(position_, new_position,
-                           provider == position_provider_))
-    return;
-  position_provider_ = provider;
-  position_ = new_position;
-  arbitrator_update_callback_.Run(position_);
+  if (IsNewPositionBetter(position_, new_position,
+                           provider == position_provider_)) {
+    position_provider_ = provider;
+    position_ = new_position;
+  }
+  if (providers_results_count_.size() >= providers_.size())
+    arbitrator_update_callback_.Run(position_);
 }
 
 AccessTokenStore* LocationArbitratorImpl::NewAccessTokenStore() {
@@ -159,7 +162,7 @@ LocationProvider* LocationArbitratorImpl::NewNetworkLocationProvider(
 }
 
 LocationProvider* LocationArbitratorImpl::NewSystemLocationProvider() {
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+#if defined(OS_LINUX)
   return NULL;
 #else
   return content::NewSystemLocationProvider();
