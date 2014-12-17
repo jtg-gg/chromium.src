87b673d8ba72881e3f0e12921d6bd83d026eadde
Jefry <jefry.ted@vcube.co.jp>
2014-12-17 15:15:11 +0800

    [GeoLocation][MacOS][WIN] system location provider implementation, disable network location provider

--- services/device/geolocation/location_arbitrator.cc
+++ services/device/geolocation/location_arbitrator.cc
@@ -58,7 +58,7 @@ void LocationArbitrator::StartProvider(bool enable_high_accuracy) {
 
   if (providers_.empty()) {
     RegisterSystemProvider();
-    RegisterNetworkProvider();
+    //RegisterNetworkProvider();
   }
   DoStartProviders();
 }
@@ -156,7 +156,7 @@ LocationArbitrator::NewNetworkLocationProvider(
 
 std::unique_ptr<LocationProvider>
 LocationArbitrator::NewSystemLocationProvider() {
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX) || \
+#if defined(OS_LINUX) || \
     defined(OS_FUCHSIA)
   return nullptr;
 #else
