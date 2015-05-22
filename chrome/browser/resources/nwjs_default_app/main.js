fc016abdda8f0f75a129aed360352f59455ec63c
Roger <wenrui@gmail.com>
2015-05-22 13:58:40 +0800

    support --url in default app

--- chrome/browser/resources/nwjs_default_app/main.js
+++ chrome/browser/resources/nwjs_default_app/main.js
@@ -1,5 +1,7 @@
+var manifest = chrome.runtime.getManifest();
+var url = manifest.cmdlineUrl || 'nw_blank.html';
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
-      'nw_blank.html',
+      url,
       {'id': 'nwjs_default_app', 'height': 550, 'width': 750});
 });
