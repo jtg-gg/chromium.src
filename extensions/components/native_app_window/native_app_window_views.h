c086804dc9b5e3eb7c4ae051c61e2235848f3a9b
Roger Wang <roger.wang@intel.com>
2015-07-22 12:01:23 +0800

    expose layout method of native_app_window_views

--- extensions/components/native_app_window/native_app_window_views.h
+++ extensions/components/native_app_window/native_app_window_views.h
@@ -66,6 +66,7 @@ class NativeAppWindowViews : public extensions::NativeAppWindow,
   void set_window_for_testing(views::Widget* window) { widget_ = window; }
   void set_web_view_for_testing(views::WebView* view) { web_view_ = view; }
 
+  void layout_() { Layout(); }
  protected:
   // Initializes |widget_| for |app_window|.
   virtual void InitializeWindow(
