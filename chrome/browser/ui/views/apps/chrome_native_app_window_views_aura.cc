ce5ff6a79190f1b98902113a2162bcfa02447557
Jefry <jefry.ted@vcube.co.jp>
2016-02-03 09:08:55 +0700

    [Drag Region][WIN] force_enable_drag_region implementation

--- chrome/browser/ui/views/apps/chrome_native_app_window_views_aura.cc
+++ chrome/browser/ui/views/apps/chrome_native_app_window_views_aura.cc
@@ -324,7 +324,7 @@ ChromeNativeAppWindowViewsAura::CreateNonClientFrameView(
         ->SetDelegate(scoped_ptr<ash::wm::WindowStateDelegate>(
             new NativeAppWindowStateDelegate(app_window(), this)));
 
-    if (IsFrameless())
+    if (IsFrameless() || IsForceEnableDragRegion())
       return CreateNonStandardAppFrame();
 
     if (app_window()->window_type_is_panel()) {
