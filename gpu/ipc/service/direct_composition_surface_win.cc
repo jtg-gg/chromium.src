5fe8c53feb8216fe4d5b3fab6e9c3f2c837d590b
Jefry <jefry.ted@vcube.co.jp>
2018-09-17 12:06:12 +0700

    [Media Recorder]  fix MacOS debug assert on cr75
    Squashed commits:
    
    [20492319c104] [Media Recorder] Windows debug assert fixes for cr72
    [43b6e1e2463f] [Media Recorder] render screen capture using gpu [WIN]
    [795b805] add javascript resources,
    adjust event dispatcher, enable audio capture for non screen capture streams,
    pending data might contain negative value, cast it to int so we can get the actual audio delay,
    [WIN] fix timestamp calculation, disable audio loopback on XP,
    [WIN] force audio share on window content,
    suppress media filter logs
    [ec9d1e713605] "fix" cr69 fail getting audio stream

--- gpu/ipc/service/direct_composition_surface_win.cc
+++ gpu/ipc/service/direct_composition_surface_win.cc
@@ -690,9 +690,9 @@ bool DCLayerTree::InitializeVideoProcessor(const gfx::Size& input_size,
     // This can fail if the D3D device is "Microsoft Basic Display Adapter".
     if (FAILED(d3d11_device_.As(&video_device_))) {
       DLOG(ERROR) << "Failed to retrieve video device from D3D11 device";
-      return false;
+      //return false;
     }
-    DCHECK(video_device_);
+    //DCHECK(video_device_);
 
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context;
     d3d11_device_->GetImmediateContext(&context);
