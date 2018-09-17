47ca73640b6e7676acbf14ffb7714b350d6c655c
Jefry <jefry.ted@vcube.co.jp>
2018-09-17 12:06:12 +0700

    [Media Recorder] Windows debug assert fixes for cr72 Squashed commits: [43b6e1e2463f] [Media Recorder] render screen capture using gpu [WIN] Squashed commits: [795b805] add javascript resources, adjust event dispatcher, enable audio capture for non screen capture streams, pending data might contain negative value, cast it to int so we can get the actual audio delay, [WIN] fix timestamp calculation, disable audio loopback on XP, [WIN] force audio share on window content, suppress media filter logs [ec9d1e713605] "fix" cr69 fail getting audio stream

--- gpu/ipc/service/direct_composition_surface_win.cc
+++ gpu/ipc/service/direct_composition_surface_win.cc
@@ -595,9 +595,9 @@ bool DCLayerTree::InitializeVideoProcessor(const gfx::Size& input_size,
     // This can fail if the D3D device is "Microsoft Basic Display Adapter".
     if (FAILED(d3d11_device_.CopyTo(video_device_.GetAddressOf()))) {
       DLOG(ERROR) << "Failed to retrieve video device from D3D11 device";
-      return false;
+      //return false;
     }
-    DCHECK(video_device_);
+    //DCHECK(video_device_);
 
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context;
     d3d11_device_->GetImmediateContext(context.GetAddressOf());
