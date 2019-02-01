92cb34943fef09ce17a28a157109db4e35dab3a6
Jefry <jefry.ted@vcube.co.jp>
2019-02-01 12:36:59 +0700

    [Build] [MacOS] enable crash_reporter, suppress stderr dump sym log

--- build/redirect_stdout.py
+++ build/redirect_stdout.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 
 import subprocess
 import sys
+import os
 
 # This script executes a command and redirects the stdout to a file. This is
 # equivalent to |command... > output_file|.
@@ -18,4 +19,5 @@ if __name__ == '__main__':
     sys.exit(1)
 
   with open(sys.argv[1], 'w') as fp:
-    sys.exit(subprocess.check_call(sys.argv[2:], stdout=fp))
+    fnull = open(os.devnull, 'w')
+    sys.exit(subprocess.check_call(sys.argv[2:], stdout=fp, stderr=fnull))
